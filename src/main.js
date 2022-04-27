586e8b9a589b102bda32ee9ff4eec2f71dd202b4
oracle <zhangyongmg@126.com>
2022-04-27 18:42:22 +0800

    增加cssloader，html生成插件，server

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+require('./css/main.css');
 // 通过 CommonJS 规范导入 show 函数
 const show = require('./show.js');
 // 执行 show 函数
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,38 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   // JavaScript 执行入口文件
   entry: './src/main.js',
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
-    filename: '[name]_[hash:8].js',
+    filename: '[name]_[chunkhash:8].js',
     // 输出文件都放到 dist 目录下
     path: path.resolve(__dirname, './build'),
-  }
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: 'body'
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: `css/[name]_[contenthash:8].css`
+    })
+  ]
 };
